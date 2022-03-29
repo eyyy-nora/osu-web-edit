a501a6e687efae3dd0bf69601ae394257d6f12f2
Merlin Reichwald <merlin.reichwald@gmail.com>
2022-03-29 19:47:03 +0200

    chore: add basic beatmap parsing entry point

--- src/client/hello.ts
+++ src/client/hello.ts
@@ -1,6 +1,17 @@
 import axios from "axios";
+import jszip from "jszip";
 
 export async function hello(): Promise<{ message: string }> {
   const { data } = await axios.get("/.netlify/functions/hello");
   return data;
+}
+
+
+export async function parse(beatmap = "/DotEXE - Battlecry.osz") {
+  const zip = await jszip.loadAsync(beatmap);
+  for (const [fileName, entry] of Object.entries(zip.files)) {
+    if (/\.osu$/.test(fileName)) {
+      const content = await entry.async("string");
+    }
+  }
 }
