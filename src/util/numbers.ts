7eef38bab27fcd027600e3946e08b1495e0ef8f8
abraker <abraker95@gmail.com>
2022-04-03 14:38:12 -0400

    Add helper functions for bezier calc

--- src/util/numbers.ts
+++ src/util/numbers.ts
@@ -19,6 +19,32 @@ export function approachRateToMs(ar: number): [preempt: number, fadein: number] 
   ];
 }
 
+export function prod(values: number[]): number {
+  return values.reduce((a, b) => a*b, 1);
+}
+
+export function lerp(low: number, high: number, percent: number): number {
+  return low*(1.0 - percent) + high*percent;
+}
+
+export function binomialCoefficient(n: number, k: number): number {
+  if (k < 0 || k > n) return 0;
+  if (k === 0 || k === n) return 1;
+
+  // Take advantage of geometry
+  k = Math.min(k, n - k);
+
+  // [0 ... k]
+  let k_range = Array.from(Array(k).keys());
+
+  // prod((n - k_range[]) / (k_range[] + 1))
+  return prod(k_range.map((x) => (n - x) / (x + 1)));
+}
+
+export function bernstein(i, n, t): number {
+  return binomialCoefficient(n, i) * Math.pow(t, i) * Math.pow(1 - t, n - i);
+}
+
 export const BIT0 = 1;
 export const BIT1 = 2;
 export const BIT2 = 4;
