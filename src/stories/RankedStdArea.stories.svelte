11102a3c7395f1957348e5a0ee6d0094eb531a4f
Merlin Reichwald <merlin.reichwald@gmail.com>
2022-04-03 17:45:10 +0200

    chore: story for ranked area

---
+++ src/stories/RankedStdArea.stories.svelte
@@ -0,0 +1,24 @@
+<script>
+import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+import OsuEditorRankedArea from "../owo/OsuEditorRankedArea.svelte";
+import OsuEditorStdMapView from "../owo/OsuEditorStdMapView.svelte";
+import ScreenBox from "../component/layout/ScreenBox.svelte";
+</script>
+
+
+<Meta
+  title="rendered/std"
+  component={OsuEditorRankedArea}
+/>
+
+<Template let:args>
+  <ScreenBox>
+    <OsuEditorStdMapView>
+      <OsuEditorRankedArea {...args} />
+    </OsuEditorStdMapView>
+  </ScreenBox>
+</Template>
+
+<Story
+  name="RankedArea"
+/>
