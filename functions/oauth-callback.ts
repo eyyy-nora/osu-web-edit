169b3096f4829f26e684301b5bab2656dd3dbb07
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-04-04 11:24:13 -0300

    fix: Rename handler

--- functions/oauth-callback.ts
+++ functions/oauth-callback.ts
@@ -3,7 +3,7 @@ import { Handler } from "@netlify/functions";
 const clientId = process.env.OSU_CLIENT_ID;
 const token = process.env.OSU_SECRET;
 
-export const oAuthHandler: Handler = async (event, context) => {
+export const handler: Handler = async (event, context) => {
   let code = (event.queryStringParameters === null) ? "" : event.queryStringParameters.code;
   return {
     statusCode: 200,
