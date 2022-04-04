92d597275b52abc7594842259d22e107330d2750
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-04-04 11:18:17 -0300

    chore: Display 'code' parameter #8

--- functions/oauth-callback.ts
+++ functions/oauth-callback.ts
@@ -3,11 +3,10 @@ import { Handler } from "@netlify/functions";
 const clientId = process.env.OSU_CLIENT_ID;
 const token = process.env.OSU_SECRET;
 
-export const handler: Handler = async (event, context) => {
-  let code = event.queryStringParameters;
-
+export const oAuthHandler: Handler = async (event, context) => {
+  let code = (event.queryStringParameters === null) ? "" : event.queryStringParameters.code;
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `queryString parameters = ${code}` }),
+    body: JSON.stringify({ message: `Your osu! OAuthCode is = ${code}` }),
   }
 }
