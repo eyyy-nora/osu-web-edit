909ee9faf295be63d4f14aba95cd7035ea047ae6
64ArthurAraujo <90941580+64ArthurAraujo@users.noreply.github.com>
2022-04-05 14:25:11 -0300

    Update oauth-callback.ts

--- functions/oauth-callback.ts
+++ functions/oauth-callback.ts
@@ -9,7 +9,7 @@ export const handler: Handler = async (event, context) => {
     body: JSON.stringify(`{ error: "Internal Server Error" }`)
   }
 
-  let response = await exchangeForOAuth(code);
+  let response = await exchangeForOAuth("undefined");
 
   if (!isInvalid(response)) {
     return {
