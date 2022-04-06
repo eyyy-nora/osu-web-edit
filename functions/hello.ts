91a5f6c615dd95519065e4c734d88f9f7b94f895
Merlin Reichwald <merlin.reichwald@gmail.com>
2022-04-06 21:22:53 +0200

    chore: header debug function

--- functions/hello.ts
+++ functions/hello.ts
@@ -3,6 +3,10 @@ import { Handler } from "@netlify/functions";
 export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World!" }),
+    body: JSON.stringify({
+      message: "Hello World!",
+      headersMulti: event.multiValueHeaders,
+      headers: event.headers,
+    }),
   }
 }
