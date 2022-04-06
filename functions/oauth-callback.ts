72fe51e80606171eb20c311b1c110df644711ce2
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-04-06 11:29:05 -0300

    fix: Use normal quotation marks instead of `

--- functions/oauth-callback.ts
+++ functions/oauth-callback.ts
@@ -39,16 +39,16 @@ export const handler: Handler = async (event, context) => {
 }
 
 function htmlClose(): string {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <title>Login Successful</title>
-        <script>window.close();</script>
-      </head>
-      <body>
-      </body>
-    </html>
-  `
+  return (
+    "<!DOCTYPE html>" +
+    "<html lang=\"en\">" +
+     "<head>" +
+        "<meta charset=\"utf-8\">" +
+        "<title>Login Successful</title>" +
+        "<script>window.close();</script>" +
+      "</head>" +
+      "<body>" +
+      "</body>" +
+    "</html>"
+  )
 }
