0ab7f59487919424e24baee9b866cf77910f8057
Merlin Reichwald <merlin.reichwald@gmail.com>
2022-04-14 00:40:21 +0200

    chore: remove example api call

--- src/client/hello.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function hello(): Promise<{ message: string }> {
-  const { data } = await axios.get("https://owe.monster/.netlify/functions/hello");
-  return data;
-}
