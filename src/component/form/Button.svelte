78e47f44424bbf9b04352f8ba05150d70a7278e4
Arthur Araujo <arthur.araujo@tutanota.com>
2022-08-06 00:12:51 -0300

    fix: Missing disabled property in button component

--- src/component/form/Button.svelte
+++ src/component/form/Button.svelte
@@ -1,52 +1,56 @@
 <script lang="ts">
-import Icon from "src/component/general/Icon.svelte";
-import { OwoId } from "src/icons";
+  import Icon from "src/component/general/Icon.svelte";
+  import { OwoId } from "src/icons";
 
-export let text: string = undefined;
-export let icon: OwoId = undefined;
-export let big: boolean = false;
-export let pad: boolean = false;
-export let inline: boolean = false;
-
+  export let disabled: boolean = false;
+  export let text: string = undefined;
+  export let icon: OwoId = undefined;
+  export let big: boolean = false;
+  export let pad: boolean = false;
+  export let inline: boolean = false;
 </script>
 
-<button type="button" class:big class:pad class:inline on:click|stopPropagation>
-  <slot>
-    {#if icon}<Icon {icon} />{/if}
-    {#if text}<span>{text}</span>{/if}
-  </slot>
-</button>
+{#if !disabled}
+  <button type="button" class:big class:pad class:inline on:click|stopPropagation>
+    <slot>
+      {#if icon}<Icon {icon} />{/if}
+      {#if text}<span>{text}</span>{/if}
+    </slot>
+  </button>
+{/if}
 
 <style>
-button {
-  max-height: 1.6rem;
-  box-sizing: border-box;
-  padding: .1rem .5rem;
-  border: 1px solid var(--colorFgRegular);
-  color: var(--colorFgLight);
-  outline: none;
-  background-color: var(--colorBgLighter);
-  cursor: pointer;
-}
+  button {
+    max-height: 1.6rem;
+    box-sizing: border-box;
+    padding: 0.1rem 0.5rem;
+    border: 1px solid var(--colorFgRegular);
+    color: var(--colorFgLight);
+    outline: none;
+    background-color: var(--colorBgLighter);
+    cursor: pointer;
+  }
 
-button.inline {
-  border: none;
-  background-color: transparent;
-  color: inherit;
-  margin: 0;
-}
+  button.inline {
+    border: none;
+    background-color: transparent;
+    color: inherit;
+    margin: 0;
+  }
 
-button.pad {
-  margin: .2rem .3rem;
-}
+  button.pad {
+    margin: 0.2rem 0.3rem;
+  }
 
-button.big {
-  max-height: 2.4rem;
-  padding: .3rem .5rem;
-}
+  button.big {
+    max-height: 2.4rem;
+    padding: 0.3rem 0.5rem;
+  }
 
-button:focus, button:active, button:hover {
-  color: var(--colorFgLighter);
-  background-color: var(--colorBgLightest);
-}
+  button:focus,
+  button:active,
+  button:hover {
+    color: var(--colorFgLighter);
+    background-color: var(--colorBgLightest);
+  }
 </style>
--- src/owo/OsuAddLayerDialog.svelte
+++ src/owo/OsuAddLayerDialog.svelte
@@ -1,46 +1,44 @@
 <script lang="ts">
-import { BeatmapLayer } from "src/io";
-import { snowflake } from "src/util/snowflake";
-import { tick } from "svelte";
-import Button from "../component/form/Button.svelte";
-import Input from "../component/form/Input.svelte";
-import Dialog from "src/component/page/Dialog.svelte";
-import { getMapsetContext } from "src/context/mapset-context";
+  import { BeatmapLayer } from "src/io";
+  import { snowflake } from "src/util/snowflake";
+  import Button from "../component/form/Button.svelte";
+  import Input from "../component/form/Input.svelte";
+  import Dialog from "src/component/page/Dialog.svelte";
+  import { getMapsetContext } from "src/context/mapset-context";
 
-const { beatmap, selectLayer, toggleLayerVisible } = getMapsetContext();
+  const { beatmap, selectLayer, toggleLayerVisible } = getMapsetContext();
 
-export let open: boolean = false;
-export let layerName: string = "";
-let dialog: Dialog;
-let nameInput: Input;
+  export let open: boolean = false;
+  export let layerName: string = "";
+  let dialog: Dialog;
+  let nameInput: Input;
 
-export async function show() {
-  layerName = `Layer ${($beatmap?.layers.length ?? 0) + 1}`;
-  await dialog.show();
-  nameInput.select();
-}
+  export async function show() {
+    layerName = `Layer ${($beatmap?.layers.length ?? 0) + 1}`;
+    await dialog.show();
+    nameInput.select();
+  }
 
-export function hide() {
-  dialog.hide();
-}
+  export function hide() {
+    dialog.hide();
+  }
 
-function createLayer() {
-  if (!$beatmap) return;
-  const layer: BeatmapLayer = {
-    name: layerName,
-    id: snowflake(),
-    objects: []
-  };
-  $beatmap.layers = [...$beatmap.layers, layer];
-  toggleLayerVisible(layer);
-  selectLayer(layer);
-  hide();
-}
-
+  function createLayer() {
+    if (!$beatmap) return;
+    const layer: BeatmapLayer = {
+      name: layerName,
+      id: snowflake(),
+      objects: [],
+      visible: true,
+    };
+    $beatmap.layers = [...$beatmap.layers, layer];
+    toggleLayerVisible(layer);
+    selectLayer(layer);
+    hide();
+  }
 </script>
 
 <Dialog bind:this={dialog} closable {open} heading="Create Layer">
-
   <Input bind:this={nameInput} name="layer-name" bind:value={layerName} on:submit={createLayer} />
 
   <svelte:fragment slot="footer">
